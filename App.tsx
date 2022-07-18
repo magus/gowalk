59608f8cac90f52f3fd96d99249715886d709613
magus <noah@iamnoah.com>
2022-07-18 13:13:06 -0700

    fix: buttons and logSteps recent

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import 'expo-dev-client';
 
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { SafeAreaView, Button, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { SafeAreaView, Button, StyleSheet, Text, View } from 'react-native';
 import AppleHealthKit from 'react-native-health';
 import Animated, { useSharedValue, withSpring, useAnimatedStyle } from 'react-native-reanimated';
 import * as MMKV from 'react-native-mmkv';
@@ -107,17 +107,9 @@ export default function App() {
         <Button title="Random animation!" onPress={randomize_animation} />
         <Animated.View style={[animation_style, styles.animated_view]} />
 
-        <TouchableOpacity onPress={logSteps}>
-          <Text>logSteps</Text>
-        </TouchableOpacity>
+        <Button title="logSteps" onPress={logSteps} />
 
-        <TouchableOpacity onPress={logDistance}>
-          <Text>logDistance</Text>
-        </TouchableOpacity>
-
-        <TouchableOpacity onPress={logRunning}>
-          <Text>logRunning</Text>
-        </TouchableOpacity>
+        <Button title="logDistance" onPress={logDistance} />
       </View>
     </SafeAreaView>
   );
@@ -144,40 +136,16 @@ function logDistance() {
   });
 }
 
-function logRunning() {
-  let startMs = new Date(2022, 6, 10, 1, 30, 0).getTime();
-
-  for (let i = 0; i < 10; i++) {
-    const endMs = startMs + 1000 * 60 * 5;
-    const endDate = new Date(endMs).toISOString();
-    const startDate = new Date(startMs).toISOString();
-    startMs = endMs;
-
-    let options = {
-      type: AppleHealthKit.Constants.Activities.Running, // See HealthActivity Enum
-      startDate,
-      endDate,
-      energyBurned: 50, // In Energy burned unit,
-      energyBurnedUnit: 'calorie',
-      distance: 50, // In Distance unit
-      distanceUnit: 'meter',
-    };
-
-    AppleHealthKit.saveWorkout(options, (err, results) => {
-      if (err) {
-        console.error('AppleHealthKit.saveWorkout', { err, results });
-        throw err;
-      }
-
-      console.debug('AppleHealthKit.saveWorkout', { results });
-    });
-  }
-}
-
 function logSteps() {
+  const now = new Date().getTime();
+
+  const startMs = now - 1000 * 30;
+  const endMs = startMs + 1000 * 30;
+
+  const endDate = new Date(endMs).toISOString();
+  const startDate = new Date(startMs).toISOString();
+
   const value = 100;
-  const startDate = new Date(2016, 6, 2, 6, 0, 0).toISOString();
-  const endDate = new Date(2016, 6, 2, 6, 0, 0).toISOString();
 
   AppleHealthKit.saveSteps({ value, startDate, endDate }, (err, results) => {
     if (err) {
